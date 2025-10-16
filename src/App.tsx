e5cc4157278ddd719f571ce0db5e3e176167fbb6
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2025-10-16 11:07:33 -0700

    AHHH

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl">
-      <h1>Hello, Astrii 🌌</h1>
-    </div>
-  );
-}
-
-export default App;
